69f8176308a42c9cc5055f0f62e094af39142883
Arun8933 <91746514+Arun8933@users.noreply.github.com>
2021-10-14 06:15:09 +0100

    Add files via upload
    my captain project 2

---
+++ VariableAndDatatypes.java
@@ -0,0 +1,16 @@
+
+public class VariableAndDatatypes {
+public static void main (String[]args) {
+	int x;
+	x=10;
+	int y=10;
+	System.out.println(x);
+	System.out.println(y);
+	double d = 20.8;
+	System.out.println(d);
+	boolean z=true;
+	System.out.println(true);
+	String name ="Arun";
+	
+}
+}
